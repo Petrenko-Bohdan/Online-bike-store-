d3b6fe978287d57b5724e1f0f6dfcbaed2c66c3c
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2024-04-23 18:49:48 +0200

    app other component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
-
+import { OrderComponent } from './order/order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductsComponent,  RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, ProductsComponent,  RouterLink, RouterLinkActive, OrderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
